61c55627bbc8729cd646c39c242c4a375b5e4e32
Nolan Carroll <ncarroll@customink.com>
2012-06-14 14:31:55 -0400

    fixed object error in javascript

--- tictactoe/app.js
+++ tictactoe/app.js
@@ -1,3 +1,8 @@
+var combos = [
+  [0,1,2],[3,4,5],[6,7,8],
+  [0,3,6],[1,4,7],[2,5,8],
+  [0,4,8],[2,4,6]
+]
 
 $(function() {
 
@@ -10,6 +15,7 @@ $(function() {
       $inner.text(turn);
       $('#turn').text(turn == 'O' ? 'X' : 'O');
     }
+    checkWinCondition()
     return false;
   });
 });
@@ -27,14 +33,27 @@ function addSquares($ttt) {
   for(i = 0; i < 9; i++) {
     $ttt.append('<a href="#" class="square"><div class="square-inner"></div</a>');
   }
-  $ttt.children('.square:lt(3)').css('border-bottom', '1px dashed #999');
-  $ttt.children('.square:gt(5)').css('border-top', '1px dashed #999');
-  $.each([0,3,6], function(index, item) { $ttt.children('.square:eq('+item+')').css('border-right', '1px dashed #999')});
-  $.each([2,5,8], function(index, item) { $ttt.children('.square:eq('+item+')').css('border-left', '1px dashed #999')});
+  $('.square:lt(3)').css('border-bottom', '1px dashed #999');
+  $('.square:gt(5)').css('border-top', '1px dashed #999');
+  $.each([0,3,6], function(index, item) { getSquare(item).css('border-right', '1px dashed #999')});
+  $.each([2,5,8], function(index, item) { getSquare(item).css('border-left', '1px dashed #999')});
 }
 
-var combos = [
-  [0,1,2],[3,4,5][6,7,8],
-  [0,3,6],[1,4,7],[2,5,8],
-  [0,4,8],[2,4,6]
-]
+function checkWinCondition() {
+  for(i = 0; i < combos.length; i++){
+    var combo = combos[i];
+    var initial = getSquare(combo[0]).text();
+    for(k = 1; k < combo.length; k++){
+      if(getSquare(combo[k]) != initial) continue;
+      if(k == combo.length-1) endGame(combo);
+    }
+  }
+}
+
+function getSquare(index) {
+  return $('.square:eq(' + index + ')');
+}
+
+function endGame(combo) {
+  console.log(combo);
+}
