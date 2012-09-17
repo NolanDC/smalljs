96ed3674501e87359dcd4f90fb79682e0a1a33f3
Nolan Carroll <nolandcarroll@gmail.com>
2012-09-16 22:50:29 -0400

    nolandc.com

---
+++ nolancarroll/app.js
@@ -0,0 +1,7 @@
+$(function() {
+	//$('#squares').css({'margin-left': -$('#squares').width()/2});	
+	$('#rectangles').hide().fadeIn('slow');
+	$('.information.fill').prepend('<a href="mailto:nolandcarroll@gmail.com">email</a>');
+
+})
+  
